86a37d4e9663f1461d100b86ef236a96a6b86e82
Brandon Campbell <brandon.william.campbell@gmail.com>
2017-07-22 14:34:06 -0500

    db, controller, hb changes

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+insert into burgers_tbl(burger_name) values ('Cheeseburger');
+insert into burgers_tbl(burger_name) values ('Double Bacon Cheeseburger');
+insert into burgers_tbl(burger_name) values ('Hamburger');
+insert into burgers_tbl(burger_name) values ('Roasted Chili Pepper Burger');
